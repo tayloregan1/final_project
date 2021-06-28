a72ce3681589edfb8e90a9f6aba55acdbd98efa3
Anusha Patel <anushahp@outlook.com>
2021-06-27 20:10:58 -0400

    main python script

---
+++ app.py
@@ -0,0 +1,16 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import streamlit as st
+from openpyxl import load_workbook
+st.write('Hello')
+
+
+# %%
+1+1
+
+
+# %%
+
+
+
