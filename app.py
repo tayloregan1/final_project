b70e7d2a81850f5b8553ceafd3b8ffeb37af7129
tayloregan1 <82550628+tayloregan1@users.noreply.github.com>
2021-06-29 16:36:42 -0400

    Update app.py with dataframe

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 
 @st.cache
@@ -11,27 +12,49 @@ def load_data(filename):
 shark = load_data('cleaned_sharks.csv')
 sharkattack_counts = load_data('sharkattack_counts.csv')
 
+# def bar_plot(df, column1, column2): # Set x equal to place names
+x = sharkattack_counts['Year']
+bar_width = 0.75
+fig, ax = plt.subplots()
+# x-axis is place names, y-axis is attendance
+ax.bar(x, sharkattack_counts['Amount of Shark Attacks'], width=bar_width, color='pink', edgecolor='blue')
+# Set x-ticks to be place names
+ax.set_xticks(x)
+ax.set_xticks(sharkattack_counts['Year'])
+plt.xticks(rotation=90)
+# Set y-ticks to be more uniform plt.yticks(np.arange(1000000, 8500000, step=1000000)) # Add a grid
+ax.grid(axis='y', linestyle='--')
+# Add a title
+plt.title('Amount of Shark Attacks per Year') # Add x label and y label
+plt.xlabel('Year')
+plt.ylabel('Amount of Shark Attacks')
+# Display bar plot
+st.pyplot(fig)
+
 # sidebar
 st.sidebar.title('Filter Data Here:')
-
-min_year, max_year = st.sidebar.slider('Years to include:', 1961, 2020, (2000, 2020), 1)
-min_age, max_age = st.sidebar.slider('Ages to include:', 3, 87, (18, 35), 1)
-countries = st.sidebar.multiselect('Areas to include:', sorted(shark['Country'].unique()))
-sex = st.sidebar.multiselect('Sexes to include:', ['M', 'F'])
-fatal = st.sidebar.multiselect('Fatality type to include:', [True, False])
-
 st.header('Exploring Shark Attack Data')
 
-# years = range(min_year, max_year + 1)
-# ages = range(min_age, max_age + 1)
+min_year, max_year = st.sidebar.slider('Years to include:', 1961, 2020, (2000, 2020), 1)
+filtered_df = shark.loc[(shark['Year'] <= max_year) & (shark['Year'] >= min_year)]
 
-countries_list = list(countries)
-st.write(countries_list)
+min_age, max_age = st.sidebar.slider('Ages to include:', 3, 87, (18, 35), 1)
+filtered_df = filtered_df.loc[(shark['Age'] <= max_age) & (shark['Age'] >= min_age)]
 
-filtered_df = shark.loc[(shark['Year'] <= max_year) & (shark['Year'] >= min_year)
-                        & (shark['Age'] <= max_age) & (shark['Age'] >= min_age)]
-filter_countries = filtered_df['Country'].isin(countries_list)
-filtered_df = filtered_df[filter_countries]
+countries = shark['Country'].unique()
+countries_choice = st.sidebar.multiselect('Areas to include:', countries)
+countries_choice_list = list(countries_choice)
+filtered_df = filtered_df[shark['Country'].isin(countries_choice_list)]
+
+sex = shark['Sex'].unique()
+sex_choice = st.sidebar.multiselect('Sexes to include:', sex)
+sex_choice_list = list(sex_choice)
+filtered_df = filtered_df[shark['Sex'].isin(sex_choice_list)]
+
+fatal = shark['Fatal==True'].unique()
+fatal_choice = st.sidebar.multiselect('Types to include:', fatal)
+fatal_choice_list = list(fatal_choice)
+filtered_df = filtered_df[shark['Fatal==True'].isin(fatal_choice_list)]
 
 st.dataframe(filtered_df)
 
@@ -39,7 +62,7 @@ st.header('Percent of Shark Attacks by Type of Attack')
 unprov = len(shark[shark['Type'] == 'Unprovoked'])/len(shark)
 prov = len(shark[shark['Type'] == 'Provoked'])/len(shark)
 null = len(shark[shark['Type'].isnull()])/len(shark)
-type_dictionary = {'Provoked': prov, 'Unprovoked': unprov, 'Other':null}
+type_dictionary = {'Provoked': prov, 'Unprovoked': unprov, 'Other': null}
 types = type_dictionary.keys()
 freq = type_dictionary.values()
 pie_chart = px.pie(values=freq, names=types, color=types, title='Pie Chart: Type of Attack')
